ee8de5bb4047a98582834d532814a28cb6632197
akifumi <akifumi139@gmail.com>
2023-12-30 06:51:41 +0000

    fix:戻るボタンを追加

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -26,7 +26,22 @@
                 </div>
             </div>
         </div>
-        <main class="pt-16 pb-24 ps-2 bg-white">
+        <div class="pt-16 flex me-2">
+            <a href="{{ route('projects.show', ['no' => $project->no]) }}"
+                class="text-lg  text-teal-700 py-1 px-1 rounded-md font-bold flex items-center">
+                <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox="0 0 24 24">
+                    <title>back_2_fill</title>
+                    <g id="back_2_fill" fill='none' fill-rule='nonzero'>
+                        <path
+                            d='M24 0v24H0V0h24ZM12.593 23.258l-.011.002-.071.035-.02.004-.014-.004-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01-.017.428.005.02.01.013.104.074.015.004.012-.004.104-.074.012-.016.004-.017-.017-.427c-.002-.01-.009-.017-.017-.018Zm.265-.113-.013.002-.185.093-.01.01-.003.011.018.43.005.012.008.007.201.093c.012.004.023 0 .029-.008l.004-.014-.034-.614c-.003-.012-.01-.02-.02-.022Zm-.715.002a.023.023 0 0 0-.027.006l-.006.014-.034.614c0 .012.007.02.017.024l.015-.002.201-.093.01-.008.004-.011.017-.43-.003-.012-.01-.01-.184-.092Z' />
+                        <path fill='#0F766EFF'
+                            d='M7.16 10.972A7 7 0 0 1 19.5 15.5a1.5 1.5 0 1 0 3 0c0-5.523-4.477-10-10-10a9.973 9.973 0 0 0-7.418 3.295L4.735 6.83a1.5 1.5 0 1 0-2.954.52l1.042 5.91c.069.391.29.74.617.968.403.282.934.345 1.385.202l5.644-.996a1.5 1.5 0 1 0-.52-2.954l-2.788.491Z' />
+                    </g>
+                </svg>
+                <span class="ms-2">戻る</span>
+            </a>
+        </div>
+        <main class="pt-4 ps-2 bg-white">
             <div class="text-2xl font-light transform scaleY-120 my-0 mb-6 font-serif bg-gray-200 p-2 w-full"
                 style="font-family: 'Dela Gothic One', sans-serif;">{{ $project->title }}</div>
             <form action="{{ route('tasks.update', ['projectId' => $project->id, 'taskId' => $task->id]) }}"
