09b87d05ace27dbbc31ecfde5e9aa1cfd3e01aee
akifumi <akifumi139@gmail.com>
2024-03-03 07:47:21 +0000

    add:ブログ・YouTube・インスタのリンクボタンの配置を変更（レイアウト調整含む）

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -26,34 +26,46 @@
 </head>
 
 <body class="mx-auto bg-white text-black" style="max-width:400px;">
-    <div style="max-width: 100%;aspect-ratio: 8/11; position: relative;text-align: center;">
+    <div style="max-width: 100%;aspect-ratio: 7/8; position: relative;text-align: center;">
         <img style="width: 100%;position: absolute;" src="{{ asset('images/top-header.jpg') }}">
-        <img style="width: 52%;position: absolute;top: 50px;left: 50%;transform: translateX(-50%);"
+        <img style="width: 42%;position: absolute;top: 32px;left: 50%;transform: translateX(-50%);"
             src="{{ asset('images/IKELog.png') }}">
+        <menu class="flex gap-16 text-center" style="position: absolute;top:32%;left: 50%;transform: translateX(-50%);">
+            <a href="#">
+                <i class="fa-solid fa-b text-3xl"></i>
+            </a>
+            <a href="https://youtube.com/@IKE-zy8pb?si=j2igaAkI6yHo3hEf">
+                <i class="fa-brands fa-youtube text-3xl"></i>
+            </a>
+            <a href="https://www.instagram.com/ikelog_travel/">
+                <i class="fa-brands fa-instagram text-3xl"></i>
+            </a>
+        </menu>
     </div>
-
     <main class="mx-4 mt-2 pb-10">
-        <h1 class="text-2xl font-light transform scaleY-120 my-0 mb-6 font-serif"
+        <h1 class="text-xl md:text-2xl font-light transform scaleY-120 my-0 mb-4 font-serif"
             style="font-family: 'Dela Gothic One', sans-serif;">
             バカで、アホで、面白い、<br>
             死ぬまでにしたい100のこと！
         </h1>
-
         <div style="font-family: 'Noto Serif JP', serif;">
-            <div class="my-2 font-medium font-serif">
-                明確な目標がなくてもいい。<br>
-                誰に否定されることでもいい。<br>
-                どれだけくだらないことでもいい。<br>
-                <br>
-                自分が少しでもやりたいと思ったことを100個集めれば、
-                きっとそれは、1つに結果になり、1つの肯定になり、1つの価値になると思う。
+            <div class="font-medium font-serif">
+                <div>
+                    明確な目標がなくてもいい。<br>
+                    誰に否定されることでもいい。<br>
+                    どれだけくだらないことでもいい。<br>
+                </div>
+                <div class="mt-2">
+                    自分が少しでもやりたいと思ったことを100個集めれば、
+                    きっとそれは、1つに結果になり、1つの肯定になり、1つの価値になると思う。
+                </div>
             </div>
 
-            <div class="text-lg mt-6 pr-8 text-right font-serif">
+            <div class="text-lg mt-4 pr-6 text-right font-serif">
                 by ike
             </div>
         </div>
-        <div class="mt-8 flex flex-col gap-5">
+        <div class="mt-6 flex flex-col gap-5">
             <div class="flex justify-center items-center">
                 <a href="{{ route('projects.index') }}"
                     class="w-64 text-center bg-sky-200 shadow-md shadow-sky-400 hover:bg-sky-400 text-gray-800 font-bold tracking-widest py-2 px-5 rounded">
@@ -61,7 +73,7 @@
                 </a>
             </div>
 
-            <div class="flex justify-center items-center">
+            {{-- <div class="flex justify-center items-center">
                 <a href="https://youtube.com/@IKE-zy8pb?si=j2igaAkI6yHo3hEf" target="_blank" rel="noopener noreferrer"
                     class="w-64 text-center bg-red-200 shadow-md shadow-red-400 hover:bg-red-400 text-gray-800 font-bold py-2 px-3 rounded">
                     <i class="fa-brands fa-youtube text-lg me-2"></i>IKEログのYouTubeを見る
@@ -73,7 +85,7 @@
                     class="w-64 text-center bg-yellow-300 shadow-md shadow-yellow-500 hover:bg-yellow-500 text-gray-800 font-bold py-2 px-3 rounded">
                     <i class="fa-brands fa-instagram me-2"></i>IKEログのInstagramを見る
                 </a>
-            </div>
+            </div> --}}
         </div>
     </main>
 </body>
